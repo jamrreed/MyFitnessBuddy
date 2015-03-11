89ab38e1af96e94a67a8ef12531043e6b0026512
Randall Reed <jamrreed@gmail.com>
2015-03-11 15:20:24 -0500

    Namespace change and nutrition page changes.

--- MyFitnessBuddy/MyFitnessBuddy/Controllers/HomeController.cs
+++ MyFitnessBuddy/MyFitnessBuddy/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace ASP.NET_MVC5_Bootstrap3_3_1_LESS.Controllers
+namespace MyFitnessBuddy.Controllers
 {
     public class HomeController : Controller
     {
@@ -36,6 +36,11 @@ namespace ASP.NET_MVC5_Bootstrap3_3_1_LESS.Controllers
         public ActionResult Nutrition()
         {
             NutritionModel model = new NutritionModel();
+            model.Foods.Add(new MyFitnessBuddy.Food() { nCalories = 50, nFat = 9, nCarb = 2, nProtein = 2 });
+            model.Foods.Add(new MyFitnessBuddy.Food() { nCalories = 35, nFat = 0, nCarb = 3, nProtein = 5 });
+            model.Foods.Add(new MyFitnessBuddy.Food() { nCalories = 125, nFat = 0, nCarb = 12, nProtein = 25 });
+            model.Foods.Add(new MyFitnessBuddy.Food() { nCalories = 200, nFat=15, nCarb = 30, nProtein = 15 });
+            model.Foods.Add(new MyFitnessBuddy.Food() { nCalories = (decimal)158.5, nFat = 20, nCarb = 5, nProtein = 20 });
 
             return View(model);
         }
--- MyFitnessBuddy/MyFitnessBuddy/Startup.cs
+++ MyFitnessBuddy/MyFitnessBuddy/Startup.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.Owin;
 using Owin;
 
-[assembly: OwinStartupAttribute(typeof(ASP.NET_MVC5_Bootstrap3_3_1_LESS.Startup))]
-namespace ASP.NET_MVC5_Bootstrap3_3_1_LESS
+[assembly: OwinStartupAttribute(typeof(MyFitnessBuddy.Startup))]
+namespace MyFitnessBuddy
 {
     public partial class Startup
     {
